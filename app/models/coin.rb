fdc72d13f05d3068f9906af501590ffe3b33d616
MrTj458 <mrtj458@gmail.com>
2018-10-08 10:23:14 -0600

    Coin crud actions

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -3,5 +3,20 @@ class Coin < ApplicationRecord
 	validates_presence_of :cmc_id, :name, :symbol
 
 	has_many :watched_coins, dependent: :destroy
-	has_many :coins, through: :watched_coins
+	has_many :users, through: :watched_coins
+
+	def self.create_by_cmc_id(res)
+		match = res['data'].with_indifferent_access
+
+		coin_params = {
+			name: match[:name],
+			symbol: match[:symbol],
+			cmc_id: match[:id]
+		}
+
+		Coin.find_or_create_by(coin_params) do |coin|
+			coin.price = match[:quotes][:USD][:price]
+			coin.last_fetched = DateTime.now
+		end
+	end
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
+import axios from 'axios'
 
 class Home extends Component {
+  state = { coins: {} }
+
+  click = () => {
+    axios.post('/api/coins?coin=btc')
+      .then( res => { debugger })
+  }
+  
   render() {
     return (
-      <Header as="h1" textAlign="center">Home Component</Header>
+      <Button onClick={this.click}>
+        Click me!
+      </Button>
     );
   }
 }
