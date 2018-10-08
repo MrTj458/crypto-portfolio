5d0141347433c5c7457b929aa2092a49345189b5
MrTj458 <mrtj458@gmail.com>
2018-10-08 10:26:07 -0600

    Removed tests on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,18 +3,10 @@ import { Header, Button } from 'semantic-ui-react';
 import axios from 'axios'
 
 class Home extends Component {
-  state = { coins: {} }
-
-  click = () => {
-    axios.post('/api/coins?coin=btc')
-      .then( res => { debugger })
-  }
-  
   render() {
     return (
-      <Button onClick={this.click}>
-        Click me!
-      </Button>
+      <>
+      </>
     );
   }
 }
